4ca5d02fbc8fd6d7e1ef9a1876b02d5302627fcd
Thiago Felix <78563115+thiagosfelix@users.noreply.github.com>
2021-05-09 00:16:53 -0300

    aulas iniciais

---
+++ http/app.js
@@ -0,0 +1,6 @@
+var http = require("http");
+
+http.createServer(function(requisicao, resposta) {
+    resposta.end("<h1>Bem-vindo ao meu site!</h1><h4>Guia do Programador</h4>");
+}).listen(8181);
+console.log("Meu servidor está rodando!");
---
+++ inicio/app.js
@@ -0,0 +1,43 @@
+var calculadora = require("./calculadora");
+
+
+console.log(calculadora.mult(10,20));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* var mostrarSite = true;
+const site = "www.guiadoprogramador.com";
+
+console.log("Hello World!");
+console.log("Meu nome é Thiago!");
+console.log('Eu estou aprendendo Node.js com o Guia do Programador');
+
+if(mostrarSite) { // Se mostrarSite
+    console.log(site); // Printe o site
+} */
